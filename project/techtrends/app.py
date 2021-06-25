8dc319c9484aa12dfb5eeec6f5bf478d696ff7a6
NickWoodUK <nickwood@gmail.com>
2021-06-25 16:21:15 -0500

    exercise one complete

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,7 +1,11 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
-from werkzeug.exceptions import abort
+from multiprocessing import Value
+
+counter = Value('i', 0)
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -10,42 +14,61 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
+    counter.value += 1
     return post
+
+
+def get_number_of_articles():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    counter.value += 1
+    return len(posts)
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    counter.value += 1
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.info('Article "' + str(post['title'] + '" retrieved!'))
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us Page retrieved!')
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -57,14 +80,47 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+                               (title, content))
             connection.commit()
             connection.close()
+            counter.value += 1
+
+            app.logger.info('Article "' + title + '" created!')
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
-# start the application on port 3111
+
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    app.logger.info('Status request successful')
+    return response
+
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    number_of_articles = get_number_of_articles()
+    metric_text = '{"db_connection_count": ' + str(counter.value) + ', "post_count": ' + str(number_of_articles) + '}'
+
+    response = app.response_class(
+        response=json.dumps(metric_text),
+        status=200,
+        mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successful')
+    return response
+
+
+# start the application on port 3111ß
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+    app.run(host='127.0.0.1', port='3111')
